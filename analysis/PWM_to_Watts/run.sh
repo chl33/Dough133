7159a70ea3239dd3eaa0da8cc8c20c642f57a4fb
Chris Lee <lee.chris.h@gmail.com>
2026-02-12 07:22:02 -0500

    improved script for running pwm analysis

--- analysis/PWM_to_Watts/run.sh
+++ analysis/PWM_to_Watts/run.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
+set -e
+# Build venv for running script, if needed.
 if [ ! -d venv ]; then
     python3 -m venv venv && ./venv/bin/pip install pandas plotly numpy scikit-learn
 fi
-
+# Run the analysis.
 ./venv/bin/python analyze_pwm.py
- 
+# Possibly copy the plot to be published.
+DEST=$HOME/Home/Blog/projects/static/json/
+if [ "$DEST" ]; then
+    cp pwm_to_watts_plot.json "$DEST"
+fi
